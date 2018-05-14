708af8a9f128fa7c5aff6624636ecd742f76182b
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2018-05-15 00:50:25 +0300

    interface

--- src/main/BSTreeInterface.java
+++ src/main/BSTreeInterface.java
@@ -1,5 +1,74 @@
 package main;
 
 public interface BSTreeInterface {
+	/**
+	 * Returns the value associated with ​key​.
+	 * @return value
+	 */
+	public V get(K ​key​);
+	/**
+	 * Adds ​value​ to the tree with respect to ​key​.
+	 */
+	public void add(K ​key​, V ​value​);
+	/**
+	 * Removes the value associated with ​key​.
+	 * @param key
+	 * @param value
+	 */
+	public void update(K key, V value);
+	/**
+	 * Returns all the keys in the tree.
+	 * @return list of keys
+	 */
+	public List<K> keys();
+	/**
+	 * Returns all the values in the tree.
+	 * @return list of values
+	 */
+	public List<V> values();
+	/**
+	 * Returns the value associated with the smallest key.
+	 * @return minimum value
+	 */
+	public V min();
+	/**
+	 * Returns the value associated with the largest key.
+	 * @return maximum value
+	 */
+	public V max();
+	/**
+	 * Returns all the values associated with a smaller key than ​key​.
+	 * @return list of values
+	 */
+	public List<V> lessThan(K ​key​);
+	/**
+	 * Returns all the values associated with a larger key than ​key​.
+	 * @return list of values
+	 */
+	public List<V> greaterThan(K ​key​);
+	/**
+	 * Returns the number of values (or keys) in the tree.
+	 * @return an integer of size
+	 */
+	public int size();
+	/**
+	 * Returns the maximum number of values that appear between the
+root and any leaf (root and leaf are counted as well)
+	 * @return an integer of height
+	 */
+	public int height();
+	/**
+	 * Prints the tree to the console.
+	 */
+	public void display();
+	/**
+	 * Determines whether the tree is full (it is full if and only if every
+node other than leaves has exactly two children and all the leaves are
+on the same level)
+	 * @return boolean
+	 */
+	public boolean isFullTree();
+	
+}	
 
-}
+	
