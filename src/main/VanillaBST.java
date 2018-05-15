027cd098b977835752a9b645514b5a0f411c0672
Zekihan <38537758+Zekihan@users.noreply.github.com>
2018-05-15 22:23:53 +0300

    n

--- src/main/VanillaBST.java
+++ src/main/VanillaBST.java
@@ -3,81 +3,81 @@ package main;
 
 public class VanillaBST<K extends Comparable<K>, V> implements BST<K, V> {
 
-	@Override
 	public V get(K key) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
-	@Override
+
 	public void add(K key,V value) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
-	@Override
+
 	public void update(K key, V value) {
-		// TODO Auto-generated method stub
+
 		
 	}
 
-	@Override
+	
 	public K[] keys() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
+	
 	public V[] values() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
+	
 	public V min() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
-	@Override
+	
 	public V max() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
+	
 	public V[] lessThan(K key) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
+	
 	public V[] greaterThan(K key) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
+	
 	public int size() {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
 
-	@Override
+	
 	public int height() {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
 
-	@Override
+	
 	public void display() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
-	@Override
+	
 	public boolean isFullTree() {
-		// TODO Auto-generated method stub
+
+		
 		return false;
 	}
 
