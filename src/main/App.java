72c6de720fb86b79578db617b8c5c8c37dcab92f
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2018-05-17 23:10:22 +0300

    better test

--- src/main/App.java
+++ src/main/App.java
@@ -5,13 +5,16 @@ public class App {
 	public static void main(String[] args) {
 		
 		BST<Integer,String> Bst = new VanillaBST<Integer,String>();
-		Bst.add(10, "a");
-		Bst.add(5, "b");
-		Bst.add(15, "o​");
-		Bst.add(3, "orta​");
-		Bst.add(4, "orta​");
-		Bst.add(2, "orta​");
-		Bst.display();
+		
+		Bst.add(10, "10");
+		Bst.add(5, "5");
+		Bst.add(15, "15");
+		Bst.add(3, "3");
+		Bst.add(7, "7");
+		Bst.add(12, "12");
+		Bst.add(17, "17");
+		
+		System.out.println(Bst.lessThan(17).size());
 	}
 
 }
